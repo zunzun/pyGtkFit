ca5495cba2b7c08a4f741c9a27b5fc81274a3588
zunzun <zunzun@zunzun.com>
2016-07-19 07:22:02 -0500

    Use Gtk.Window class  for results viewer

--- FittingResultsViewer.py
+++ FittingResultsViewer.py
@@ -13,14 +13,145 @@ import IndividualReports
 import AdditionalInfo
 
 
-if __name__ == "__main__":
-    # first, load the fitted equation
-    pickledEquationFileName = 'pickledEquationFile'
-    equationFile = open(pickledEquationFileName, 'rb')
-    equation = pickle.load(equationFile)
-    equationFile.close()
+class ResultsWindow(Gtk.Window):
+    
+    def __init__(self, pickledEquationFileName):
+        Gtk.Window.__init__(self)
+        
+        # first, load the fitted equation
+        equationFile = open(pickledEquationFileName, 'rb')
+        equation = pickle.load(equationFile)
+        equationFile.close()
 
-    win = Gtk.Window()
+        # the "main" notebook at application top level
+        nbTopLevel = Gtk.Notebook()
+        
+        nbGraphReports = Gtk.Notebook()
+        nbTopLevel.append_page(nbGraphReports, Gtk.Label("Graph Reports"))
+
+        nbTextReports = Gtk.Notebook()
+        nbTopLevel.append_page(nbTextReports, Gtk.Label("Text Reports"))
+
+        nbSourceCodeReports = Gtk.Notebook()
+        nbTopLevel.append_page(nbSourceCodeReports, Gtk.Label("Source Code"))
+
+        nbAdditionalInfo = Gtk.Notebook()
+        nbTopLevel.append_page(nbAdditionalInfo, Gtk.Label("Additional Information"))
+        
+        self.add(nbTopLevel)
+                
+        # populate the "graph reports" top-level notebook tab
+        if equation.GetDimensionality() == 2:
+            report = IndividualReports.ModelScatterConfidenceGraph(equation)
+            nbGraphReports.append_page(report, Gtk.Label("Model With 95%Confidence"))
+        else:
+            report = IndividualReports.SurfacePlot(equation)
+            nbGraphReports.append_page(report, Gtk.Label("Surface Plot"))
+            
+            report = IndividualReports.ContourPlot(equation)
+            nbGraphReports.append_page(report, Gtk.Label("Contour Plot"))
+            
+            report = IndividualReports.ScatterPlot(equation)
+            nbGraphReports.append_page(report, Gtk.Label("Scatter Plot"))
+            
+        report = IndividualReports.AbsoluteErrorGraph(equation)
+        nbGraphReports.append_page(report, Gtk.Label("Absolute Error"))
+
+        report = IndividualReports.AbsoluteErrorHistogram(equation)
+        nbGraphReports.append_page(report, Gtk.Label("Absolute Error Histogram"))
+
+        if equation.dataCache.DependentDataContainsZeroFlag != 1:
+            report = IndividualReports.PercentErrorGraph(equation)
+            nbGraphReports.append_page(report, Gtk.Label("Percent Error"))
+
+            report = IndividualReports.PercentErrorHistogram(equation)
+            nbGraphReports.append_page(report, Gtk.Label("Percent Error Histogram"))
+
+        # populate the "text reports" top-level notebook tab
+        report = IndividualReports.CoefficientAndFitStatistics(equation)
+        nbTextReports.append_page(report, Gtk.Label("Coefficient And Fit Statistics"))
+        
+        report = IndividualReports.CoefficientListing(equation)
+        nbTextReports.append_page(report, Gtk.Label("Coefficient Listing"))
+
+        report = IndividualReports.DataArrayStatisticsReport('Absolute Error Statistics', equation.modelAbsoluteError)
+        nbTextReports.append_page(report, Gtk.Label("Absolute Error Statistics"))
+        
+        if equation.dataCache.DependentDataContainsZeroFlag != 1:
+            report = IndividualReports.DataArrayStatisticsReport('Percent Error Statistics', equation.modelPercentError)
+            nbTextReports.append_page(report, Gtk.Label("Percent Error Statistics"))
+
+        # populate the "source code" top-level notebook tab
+        report = IndividualReports.SourceCodeReport(equation, 'CPP')
+        nbSourceCodeReports.append_page(report, Gtk.Label("C++"))
+
+        report = IndividualReports.SourceCodeReport(equation, 'CSHARP')
+        nbSourceCodeReports.append_page(report, Gtk.Label("CSHARP"))
+
+        report = IndividualReports.SourceCodeReport(equation, 'VBA')
+        nbSourceCodeReports.append_page(report, Gtk.Label("VBA"))
+
+        report = IndividualReports.SourceCodeReport(equation, 'PYTHON')
+        nbSourceCodeReports.append_page(report, Gtk.Label("PYTHON"))
+
+        report = IndividualReports.SourceCodeReport(equation, 'JAVA')
+        nbSourceCodeReports.append_page(report, Gtk.Label("JAVA"))
+
+        report = IndividualReports.SourceCodeReport(equation, 'JAVASCRIPT')
+        nbSourceCodeReports.append_page(report, Gtk.Label("JAVASCRIPT"))
+
+        report = IndividualReports.SourceCodeReport(equation, 'JULIA')
+        nbSourceCodeReports.append_page(report, Gtk.Label("JULIA"))
+
+        report = IndividualReports.SourceCodeReport(equation, 'SCILAB')
+        nbSourceCodeReports.append_page(report, Gtk.Label("SCILAB"))
+
+        report = IndividualReports.SourceCodeReport(equation, 'MATLAB')
+        nbSourceCodeReports.append_page(report, Gtk.Label("MATLAB"))
+
+        report = IndividualReports.SourceCodeReport(equation, 'FORTRAN90')
+        nbSourceCodeReports.append_page(report, Gtk.Label("FORTRAN90"))
+
+        # populate the "additional information" top-level notebook tab
+        historyTextView = Gtk.TextView()
+        historyTextView.get_buffer().set_text(AdditionalInfo.history)
+        historyTextView.set_wrap_mode(2) # 2 = word wrap, using Gtk.WRAP_WORD failed in developemnt
+        scrolledHistoryText = Gtk.ScrolledWindow(hadjustment=None, vadjustment=None)
+        scrolledHistoryText.add_with_viewport(historyTextView)
+        nbAdditionalInfo.append_page(scrolledHistoryText, Gtk.Label("Fitting History"))
+        
+        authorTextView = Gtk.TextView()
+        authorTextView.get_buffer().set_text(AdditionalInfo.author)
+        authorTextView.set_wrap_mode(2) # 2 = word wrap, using Gtk.WRAP_WORD failed in developemnt
+        scrolledAuthorText = Gtk.ScrolledWindow(hadjustment=None, vadjustment=None)
+        scrolledAuthorText.add_with_viewport(authorTextView)
+        nbAdditionalInfo.append_page(scrolledAuthorText, Gtk.Label("Author History"))
+        
+        linksTextView = Gtk.TextView()
+        linksTextView.get_buffer().set_text(AdditionalInfo.links)
+        linksTextView.set_wrap_mode(0) # 0 = no wrap, using Gtk.WRAP_NONE failed in developemnt
+        scrolledLinksText = Gtk.ScrolledWindow(hadjustment=None, vadjustment=None)
+        scrolledLinksText.add_with_viewport(linksTextView)
+        nbAdditionalInfo.append_page(scrolledLinksText, Gtk.Label("Web Links"))
+
+        # populate the "list of all equations" top-level notebook tab
+        dim = equation.GetDimensionality()
+        allEquationaTextView = Gtk.TextView()
+
+        # pass the textView's textbuffer for bold, italic, etc.
+        newBuffer = IndividualReports.AllEquationReport(dim, allEquationaTextView.get_buffer())
+        allEquationaTextView.set_buffer(newBuffer)
+            
+        allEquationaTextView.set_wrap_mode(0) # 0 = no wrap, using Gtk.WRAP_NONE failed in developemnt
+        scrolledAllEquationsText = Gtk.ScrolledWindow(hadjustment=None, vadjustment=None)
+        scrolledAllEquationsText.add_with_viewport(allEquationaTextView)
+
+        nbTopLevel.append_page(scrolledAllEquationsText, Gtk.Label("List Of All Standard " + str(dim) + "D Equations"))
+
+
+
+if __name__ == "__main__":
+    win = ResultsWindow(pickledEquationFileName='pickledEquationFile')
     win.set_title("Example pyGtk - Fitting Results Viewer")
 
     win.set_size_request(800, 600) # minimum screen size
@@ -29,137 +160,6 @@ if __name__ == "__main__":
     # connect this window's exit code to the Gtk main process exit code,
     # or pyGtk itself will not know to exit when the window is closed.
     win.connect("delete-event", Gtk.main_quit)
-
-    nbTopLevel = Gtk.Notebook() # the "main" notebook at application top level
-    
-    nbGraphReports = Gtk.Notebook()
-    nbTopLevel.append_page(nbGraphReports, Gtk.Label("Graph Reports"))
-
-    nbTextReports = Gtk.Notebook()
-    nbTopLevel.append_page(nbTextReports, Gtk.Label("Text Reports"))
-
-    nbSourceCodeReports = Gtk.Notebook()
-    nbTopLevel.append_page(nbSourceCodeReports, Gtk.Label("Source Code"))
-
-    nbAdditionalInfo = Gtk.Notebook()
-    nbTopLevel.append_page(nbAdditionalInfo, Gtk.Label("Additional Information"))
-    
-    # NOTE: at the end of this file is added a Gtk.TextView list of all equations
-    
-    win.add(nbTopLevel)
-    
-    
-    # populate the "graph reports" top-level notebook tab
-    if equation.GetDimensionality() == 2:
-        report = IndividualReports.ModelScatterConfidenceGraph(equation)
-        nbGraphReports.append_page(report, Gtk.Label("Model With 95%Confidence"))
-    else:
-        report = IndividualReports.SurfacePlot(equation)
-        nbGraphReports.append_page(report, Gtk.Label("Surface Plot"))
-        
-        report = IndividualReports.ContourPlot(equation)
-        nbGraphReports.append_page(report, Gtk.Label("Contour Plot"))
-        
-        report = IndividualReports.ScatterPlot(equation)
-        nbGraphReports.append_page(report, Gtk.Label("Scatter Plot"))
-        
-    report = IndividualReports.AbsoluteErrorGraph(equation)
-    nbGraphReports.append_page(report, Gtk.Label("Absolute Error"))
-
-    report = IndividualReports.AbsoluteErrorHistogram(equation)
-    nbGraphReports.append_page(report, Gtk.Label("Absolute Error Histogram"))
-
-    if equation.dataCache.DependentDataContainsZeroFlag != 1:
-        report = IndividualReports.PercentErrorGraph(equation)
-        nbGraphReports.append_page(report, Gtk.Label("Percent Error"))
-
-        report = IndividualReports.PercentErrorHistogram(equation)
-        nbGraphReports.append_page(report, Gtk.Label("Percent Error Histogram"))
-
-
-    # populate the "text reports" top-level notebook tab
-    report = IndividualReports.CoefficientAndFitStatistics(equation)
-    nbTextReports.append_page(report, Gtk.Label("Coefficient And Fit Statistics"))
-    
-    report = IndividualReports.CoefficientListing(equation)
-    nbTextReports.append_page(report, Gtk.Label("Coefficient Listing"))
-
-    report = IndividualReports.DataArrayStatisticsReport('Absolute Error Statistics', equation.modelAbsoluteError)
-    nbTextReports.append_page(report, Gtk.Label("Absolute Error Statistics"))
-    
-    if equation.dataCache.DependentDataContainsZeroFlag != 1:
-        report = IndividualReports.DataArrayStatisticsReport('Percent Error Statistics', equation.modelPercentError)
-        nbTextReports.append_page(report, Gtk.Label("Percent Error Statistics"))
-
-    # populate the "source code" top-level notebook tab
-    report = IndividualReports.SourceCodeReport(equation, 'CPP')
-    nbSourceCodeReports.append_page(report, Gtk.Label("C++"))
-
-    report = IndividualReports.SourceCodeReport(equation, 'CSHARP')
-    nbSourceCodeReports.append_page(report, Gtk.Label("CSHARP"))
-
-    report = IndividualReports.SourceCodeReport(equation, 'VBA')
-    nbSourceCodeReports.append_page(report, Gtk.Label("VBA"))
-
-    report = IndividualReports.SourceCodeReport(equation, 'PYTHON')
-    nbSourceCodeReports.append_page(report, Gtk.Label("PYTHON"))
-
-    report = IndividualReports.SourceCodeReport(equation, 'JAVA')
-    nbSourceCodeReports.append_page(report, Gtk.Label("JAVA"))
-
-    report = IndividualReports.SourceCodeReport(equation, 'JAVASCRIPT')
-    nbSourceCodeReports.append_page(report, Gtk.Label("JAVASCRIPT"))
-
-    report = IndividualReports.SourceCodeReport(equation, 'JULIA')
-    nbSourceCodeReports.append_page(report, Gtk.Label("JULIA"))
-
-    report = IndividualReports.SourceCodeReport(equation, 'SCILAB')
-    nbSourceCodeReports.append_page(report, Gtk.Label("SCILAB"))
-
-    report = IndividualReports.SourceCodeReport(equation, 'MATLAB')
-    nbSourceCodeReports.append_page(report, Gtk.Label("MATLAB"))
-
-    report = IndividualReports.SourceCodeReport(equation, 'FORTRAN90')
-    nbSourceCodeReports.append_page(report, Gtk.Label("FORTRAN90"))
-
-
-    # populate the "additional information" top-level notebook tab
-    historyTextView = Gtk.TextView()
-    historyTextView.get_buffer().set_text(AdditionalInfo.history)
-    historyTextView.set_wrap_mode(2) # 2 = word wrap, using Gtk.WRAP_WORD failed in developemnt
-    scrolledHistoryText = Gtk.ScrolledWindow(hadjustment=None, vadjustment=None)
-    scrolledHistoryText.add_with_viewport(historyTextView)
-    nbAdditionalInfo.append_page(scrolledHistoryText, Gtk.Label("Fitting History"))
-    
-    authorTextView = Gtk.TextView()
-    authorTextView.get_buffer().set_text(AdditionalInfo.author)
-    authorTextView.set_wrap_mode(2) # 2 = word wrap, using Gtk.WRAP_WORD failed in developemnt
-    scrolledAuthorText = Gtk.ScrolledWindow(hadjustment=None, vadjustment=None)
-    scrolledAuthorText.add_with_viewport(authorTextView)
-    nbAdditionalInfo.append_page(scrolledAuthorText, Gtk.Label("Author History"))
-    
-    linksTextView = Gtk.TextView()
-    linksTextView.get_buffer().set_text(AdditionalInfo.links)
-    linksTextView.set_wrap_mode(0) # 0 = no wrap, using Gtk.WRAP_NONE failed in developemnt
-    scrolledLinksText = Gtk.ScrolledWindow(hadjustment=None, vadjustment=None)
-    scrolledLinksText.add_with_viewport(linksTextView)
-    nbAdditionalInfo.append_page(scrolledLinksText, Gtk.Label("Web Links"))
-    
-
-    # populate the "list of all equations" top-level notebook tab
-    dim = equation.GetDimensionality()
-    allEquationaTextView = Gtk.TextView()
-
-    # pass the textView's textbuffer for bold, italic, etc.
-    newBuffer = IndividualReports.AllEquationReport(dim, allEquationaTextView.get_buffer())
-    allEquationaTextView.set_buffer(newBuffer)
-        
-    allEquationaTextView.set_wrap_mode(0) # 0 = no wrap, using Gtk.WRAP_NONE failed in developemnt
-    scrolledAllEquationsText = Gtk.ScrolledWindow(hadjustment=None, vadjustment=None)
-    scrolledAllEquationsText.add_with_viewport(allEquationaTextView)
-
-    nbTopLevel.append_page(scrolledAllEquationsText, Gtk.Label("List Of All Standard " + str(dim) + "D Equations"))
-
 
     win.show_all()
     Gtk.main()
